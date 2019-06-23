5fa640d2e27967389ca82be6c31e2e0d9a4b5afb
Darren Senger <darren.senger@gmail.com>
2019-06-22 17:06:04 -0700

    first cross-sync

--- src/main.js
+++ src/main.js
@@ -3,6 +3,20 @@ import './plugins/vuetify'
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faGripLines,
+  faEdit,
+  faSave,
+  faMinus
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faEdit);
+library.add(faGripLines);
+library.add(faSave);
+library.add(faMinus);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
